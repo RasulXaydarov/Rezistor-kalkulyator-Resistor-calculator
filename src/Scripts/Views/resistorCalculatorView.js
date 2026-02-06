10863f64d082eaf35f7a84bffe6dbd68e646730d
RasulXaydarov <rasul_457@proton.me>
2026-02-06 20:22:05 +0500

    The menu to the resistor is added. The lines and colors can be changed with menu

--- src/Scripts/Views/resistorCalculatorView.js
+++ src/Scripts/Views/resistorCalculatorView.js
@@ -1,6 +1,25 @@
 import GeneralElements from "./generalElementsView";
 import colorsImage from "url:../../Images/ColorPicker.png" with {type: "image/png"};
-import resistorImage from "url:../../Images/Resistor.png" with {type: "image/png"};
+import linesThree from "url:../../Images/Lines-3.png" with {type: "image/png"};
+import linesFour from "url:../../Images/Lines-4.png" with {type: "image/png"};
+import linesFive from "url:../../Images/Lines-5.png" with {type: "image/png"};
+import linesSix from "url:../../Images/Lines-6.png" with {type: "image/png"};
+import resistorLittleImageThreeLines from "url:../../Images/ResistorLittleImages/image-resistor-3.png" with {type: "image/png"};
+import resistorLittleImageFourLines from "url:../../Images/ResistorLittleImages/image-resistor-4.png" with {type: "image/png"};
+import resistorLittleImageFiveLines from "url:../../Images/ResistorLittleImages/image-resistor-5.png" with {type: "image/png"};
+import resistorLittleImageSixLines from "url:../../Images/ResistorLittleImages/image-resistor-6.png" with {type: "image/png"};
+import resistorNoLines from "url:../../Images/ResistorNoLines.png" with {type: "image/png"};
+let colorOne, colorTwo, colorThree, colorFour;
+let button = document.querySelector("form button[class=\"submit\"]");
+let buttonsOfColors = document.querySelectorAll("div button");
+buttonsOfColors = Array.from(buttonsOfColors);
+let colorsOfTheLines = {};
+colorsOfTheLines.numberOfLines = 3;
+// const menu = document.querySelector("menu");
+const imageLinesThreeURL = new URL("../../Images/Lines-3.png", import.meta.url);
+const imageLinesFourURL = new URL("../../Images/Lines-4.png", import.meta.url);
+const imageLinesFiveURL = new URL("../../Images/Lines-5.png", import.meta.url);
+const imageLinesSixURL = new URL("../../Images/Lines-6.png", import.meta.url);
 
 //  Switch operator for converting color to the named color 
 function changeTheColorToTheNamedColor(color) {
@@ -56,8 +75,7 @@ function calculateTheResistor(data, dataForTheColors) {
     secondLine = data.theSecondLine.color.toLowerCase();
     numberOfZero = data.theNumberOfZero.color.toLowerCase();
     resistance = `${dataForTheColors.theFirstLine[`${firstLine}`]}` + dataForTheColors.theSecondLine[`${secondLine}`];
-    // resistance = +resistance;
-    // resistance = resistance * 10 ** dataForTheColors.theNumberOfZero[`${numberOfZero}`];
+
     // The resistor has four lines
     thirdLine = data.theThirdLine.color.toLowerCase();
     resistance = resistance + dataForTheColors.theThirdLine[`${thirdLine}`];
@@ -92,13 +110,17 @@ const generalElements = new GeneralElements();
 class ResistorCalculator {
     #header = document.querySelector("header");
     #parentElement = document.querySelector("main");
+    menu;
+    _rectangles;
     #horizontalRule;
     dialog;
     #countTheResistorButton;
     _resistorColors = {
+        numberOfLines: 3,
     };
     _data;
     lines;
+    _buttonsOfColors;
     theListOfTheResistance = {
         black: 0,
         brown: 1,
@@ -144,6 +166,7 @@ class ResistorCalculator {
         silver: null
     }
     _dataForColorsOfTheResistor;
+    _result;
     constructor() {
         this.theListOfTheResistanceForTheFirstLine.black = "";
         this.theListForNumberOfZero.gold = -1;
@@ -157,7 +180,64 @@ class ResistorCalculator {
             theCoefficientOfTheTemperature: this.theCoefficientOfTheTemperature
         }
     }
-    _result;
+    changeTheButtonThreeLinesToActive() {
+        this.menu = document.querySelector("section:nth-child(2) menu");
+        let buttonThreeLines = this.menu.children[0];
+
+        buttonThreeLines.classList.add("active");
+        this.addSomeLines(buttonThreeLines.attributes[0].value);
+    };
+    changeTheLinesBelowTheResistor(resistorImageLines) {
+        switch (this._resistorColors.numberOfLines) {
+            case 3:
+                resistorImageLines.children[0].src = imageLinesThreeURL;
+                break;
+            case 4:
+                resistorImageLines.children[0].src = imageLinesFourURL;
+                break;
+            case 5:
+                resistorImageLines.children[0].src = imageLinesFiveURL;
+                break;
+            case 6:
+                resistorImageLines.children[0].src = imageLinesSixURL;
+                break;
+        }
+    };
+    addEventToTheMenu() {
+        let resistorObject = this;
+        this.menu.addEventListener("click", function (e) {
+            e.preventDefault();
+            let button = e.target.closest("button");
+            let buttonActive = document.querySelector("menu .active");
+            if (buttonActive)
+                buttonActive.classList.remove("active");
+            button.classList.add("active");
+            resistorObject.addSomeLines(button.attributes[0].value);
+        });
+    }
+    addSomeLines(number) {
+        this._rectangles = document.querySelectorAll("rectangle");
+        this._rectangles = Array.from(this._rectangles);
+        this._buttonsOfColors = document.querySelectorAll("form button");
+        this._buttonsOfColors = Array.from(this._buttonsOfColors);
+        this._buttonsOfColors.splice(-1);
+        this._resistorColors.numberOfLines = +number;
+        this._rectangles.forEach(rectangle => rectangle.classList.add("hidden"));
+        this._buttonsOfColors.forEach(buttonOfColor => buttonOfColor.classList.add("hidden"));
+        let indexNumber;
+        this._rectangles.forEach(rectangle => {
+            indexNumber = this._rectangles.indexOf(rectangle) + 1;
+            if (indexNumber <= this._resistorColors.numberOfLines)
+                rectangle.classList.remove("hidden");
+        });
+        let resistorImageLines = document.querySelector("picture[data-line=\"lines\"]");
+        this.changeTheLinesBelowTheResistor(resistorImageLines);
+
+        this._buttonsOfColors.forEach(buttonOfColor => {
+            if (+buttonOfColor.attributes[2].value <= this._resistorColors.numberOfLines)
+                buttonOfColor.classList.remove("hidden");
+        });
+    };
     setAttributeOfTheHR() {
         this.#horizontalRule = document.querySelector("hr");
         this.#horizontalRule.setAttribute("noshade", "");
@@ -185,23 +265,32 @@ class ResistorCalculator {
     #addResistorCalculatorSection() {
         const html = `<section class="container" id="resistor-calculator">
             <h2>Qarshilikni o‘lchaydigan kalkulyator</h2>
-            <rectangle class="line-1"></rectangle>
-            <rectangle class="line-2"></rectangle>
-            <rectangle class="line-3"></rectangle>
-            <rectangle class="line-4"></rectangle>
-            <rectangle class="line-5"></rectangle>
-            <rectangle class="line-6"></rectangle>
-            <picture><img src="${resistorImage}" alt="Rezistorni o‘lchaydigan kalkulyatorning rasmi"></picture>
-            <form>
-                <button data-jscolor="{preset:'threeLines', onChange: 'change(this)'}" data-color="color-1"></button>
-                <button data-jscolor="{preset:'threeLines', onChange: 'change(this)'}" data-color="color-2"></button>
-                <button data-jscolor="{preset:'threeLines', onChange: 'change(this)'}" data-color="color-3"></button>
-                <button data-jscolor="{preset:'numberOfZeros', onChange: 'change(this)'}" data-color="color-4"></button>
-                <button data-jscolor="{preset: 'mistake', onChange: 'change(this)'}" data-color="color-5"></button>
-                <button data-jscolor="{preset:'theCoeffitientOfTheTemperature', onChange: 'change(this)'}" data-color="color-6"></button>
-                 <button>Qarshilik hisoblamoq</button>
-            </form>
-            <dialog>Hisobning natijasi</dialog>
+             <menu>
+                 <button data-number="3"><img src="${resistorLittleImageThreeLines}">3 ta chiziq</button>
+                 <button data-number="4"><img src="${resistorLittleImageFourLines}">4 ta chiziq</button>
+                 <button data-number="5"><img src="${resistorLittleImageFiveLines}">5 ta chiziq</button>
+                 <button data-number="6"><img src="${resistorLittleImageSixLines}">6 ta chiziq</button>
+             </menu> 
+                <rectangle class="line-1" data-line="line-1"></rectangle>
+                <rectangle class="line-2" data-line="line-2"></rectangle>
+                <rectangle class="line-3" data-line="line-3"></rectangle>
+                <rectangle class="line-4" data-line="line-4"></rectangle>
+                <rectangle class="line-5" data-line="line-5"></rectangle>
+                <rectangle class="line-6" data-line="line-6"></rectangle>
+                <picture><img src="${resistorNoLines}" alt="Rezistorni o‘lchaydigan kalkulyatorning rasmi"></picture>
+                <picture data-line="lines">
+                    <img src="${linesThree}" alt="Rezistorning rasmi" data-number="3">
+                </picture>
+                <form>
+                    <button data-jscolor="{preset:'threeLines', onChange: 'change(this)'}" data-color="color-1" data-number=\"1\"></button>
+                    <button data-jscolor="{preset:'threeLines', onChange: 'change(this)'}" data-color="color-2" data-number=\"2\"></button>
+                    <button data-jscolor="{preset:'threeLines', onChange: 'change(this)'}" data-color="color-3" data-number=\"3\"></button>
+                    <button data-jscolor="{preset:'numberOfZeros', onChange: 'change(this)'}" data-color="color-4" data-number=\"4\"></button>
+                    <button data-jscolor="{preset: 'mistake', onChange: 'change(this)'}" data-color="color-5" data-number=\"5\"></button>
+                    <button data-jscolor="{preset:'theCoeffitientOfTheTemperature', onChange: 'change(this)'}" data-color="color-6" data-number=\"6\"></button>
+                    <button>Qarshilik hisoblamoq</button>
+                </form>
+                <dialog>Hisobning natijasi</dialog>
         </section>`;
         this.#parentElement.insertAdjacentHTML("beforeend", html);
     }
@@ -224,8 +313,6 @@ class ResistorCalculator {
         buttonOfTheForm.addEventListener("click", function (e) {
             e.preventDefault();
 
-            console.log(e.target.parentElement);
-            console.log(dialog);
             this._data = Array.from(form).slice(0, -1);
             this._resistorColors = {
                 theFirstLine: {
@@ -277,8 +364,6 @@ class ResistorCalculator {
                 color = buttonColor.dataset.currentColor;
             });
             line.click();
-
-
         });
     };
 
@@ -286,16 +371,16 @@ class ResistorCalculator {
     1)Add the data to the object
     2)Calculate the resistance 
     3) The resistor can have three lines, four lines, five lines or six lines. We should calculate for any any type of the resistor
-    
+     
     1) Take the array data and set the data into the object;
     2) Change the value of the color with function
-
     
- 
+     
+     
     1) Set and calculate the resistor's data and resistance. Return the correct result for resistor
     2) Show the result in the dialog
     3)We should easly able to choose the lines
- 
+     
     */
 
     renderResistorCalculator() {
@@ -303,6 +388,8 @@ class ResistorCalculator {
         this.addTitleToTheHero();
         this.#addSectionHowToUse();
         this.#addResistorCalculatorSection();
+        this.changeTheButtonThreeLinesToActive();
+        this.addEventToTheMenu();
         this.#showTheDialog();
         this.#addHorizontalLine();
         generalElements.addScrollBehavior();
@@ -310,7 +397,6 @@ class ResistorCalculator {
         this.#getColorsOfTheInput();
         this.changeThecolorOfTheLines();
         // this.calculateTheResistor();
-
     }
 }
 
